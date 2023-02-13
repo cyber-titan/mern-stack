90ccedee70cc783f01823d67f954e5f8b8ffe25c
itisme <me@gmail.com>
2023-02-13 22:41:40 +0530

    added resume.html

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/resume", function(req, res){
+	// res.send("I Am Abhishek");
+	res.sendFile(__dirname + "/resume.html");
+});
+
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
 	console.log(`Server running on http://localhost:${port}`);
