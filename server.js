35c217c99c2420637a3a9688538786afda414e36
itisme <me@gmail.com>
2023-02-13 23:57:38 +0530

    serving css file

--- server.js
+++ server.js
@@ -10,13 +10,8 @@ app.get("/", function(req, res){
 app.get("/resume", function(req, res){
 	// res.send("I Am Abhishek");
 	res.sendFile(__dirname + "/resume.html");
-});
-
-app.get('/resume_style', (req, res) => {
-
 	res.sendFile("C:/Users/saiab/OneDrive/Desktop/Bootcamp/13-02-2023/git clone/mern-stack/resume_style.css");
-  
-  });
+});
 
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
