1a3ea04091925b26d6493a7171c37c23d6f8a682
My Username <abc@gmail.com>
2023-02-13 22:19:40 +0530

    Serving HTML file

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
-	res.send("I Am Abhishek");
+	// res.send("I Am Abhishek");
+	req.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, function(){
