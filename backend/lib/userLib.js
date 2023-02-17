aa828ee5362d9f9d425f0ee9508db4664fce8961
itisme <me@gmail.com>
2023-02-17 11:04:47 +0530

    added crud operations

--- backend/lib/userLib.js
+++ backend/lib/userLib.js
@@ -3,10 +3,10 @@ const userModel = require("../models/userModel");
 module.exports.getAllUsers = async function(callback){
     try{
         var users = await userModel.find({});
-        callback(null, users);
+        callback(null,users);
     }
     catch(err){
-        callback(err, null);
+        callback(err,null)
     }
 }
 
@@ -22,6 +22,55 @@ module.exports.createFirstUser = async function(callback){
         callback(null, user);
     }
     catch(err){
+        callback(err, null);
+    }
+}
+
+module.exports.createUser = async function(user, callback) {
+    try {
+        var newUser = new userModel(user);
+        var result = await newUser.save();
+        callback(null, result);
+    } 
+    catch (err) {
+        callback(err, null);
+    }
+}
+
+module.exports.updateUser = async function(username, data, callback) {
+    try {
+        var query ={
+            userName : username
+        };
+        var result = await userModel.updateOne(query,data);
+        callback(null, result);
+    } 
+    catch (err) {
+        callback(err, null);
+    }
+}
+
+module.exports.deleteUser = async function(username,callback)
+{
+    try{
+        var query = {
+            userName : username,
+        };
+        var result = await userModel.deleteOne(query);
+        // var result = await userModel.updateOne(query,{isDeleted : true});
+        callback(null, result);
+    }
+    catch(err)
+    {
+        callback(err,null);
+    }
+}
+
+module.exports.getUsersbyFilter = async function(filter,callback) {
+    try {
+        var user = await userModel.find(filter);
+        callback(null,user);
+    } catch (err) {
         callback(err, null);
     }
 }
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
 
+// serving static css files
+app.use(express.static(__dirname));
+
 app.get("/", function(req, res){
 	// res.send("I Am Abhishek");
 	res.sendFile(__dirname + "/index.html");
@@ -21,9 +24,6 @@ app.get("/card", function(req, res){
 	res.sendFile(__dirname + "/card.html");
 });
 
-// serving static css files
-app.use(express.static(__dirname));
-
 // to avoid some error
 mongoose.set('strictQuery', true);
 // whenever a connection establishes or an error happens it goes inside
@@ -33,21 +33,57 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err){
 	}
 	else{
 		console.log("DB Connected");
-		// TODO: don't create a user if atleast 1 user in the table
-		userLib.createFirstUser(function(err, res){
+		// TODO: don't create a user if atleast 1 user in the table // COMPLETED
+		// userLib.createFirstUser(function(err, res){
+		// 	if(err){
+		// 		userLib.getAllUsers(function(err, res){
+		// 			if(res[0] != null){
+		// 				console.log("Already " + res[0].userName + " User Exists.");
+		// 			}
+		// 		});
+		// 	}
+		// 	else{
+		// 		console.log(res);
+		// 	}
+		// });
+
+		// userLib.createUser({userName: "abhi01", yearOfGraduation: 2021}, function(err,result){
+		// 	if(err)
+		// 	{
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+
+        // userLib.deleteUser("abhi", function(err, result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+
+		// userLib.updateUser("abhishek", {userName: "abhi"}, function(err, result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		
+		userLib.getUsersbyFilter({ userName: "abhi"}, function(err, result){
 			if(err){
-				// console.log(err);
-				userLib.getAllUsers(function(err, res){
-					if(res[0] != null){
-						console.log("Already " + res[0].userName + " User Exists.");
-					}
-				});
+				console.error(err);
 			}
 			else{
-				console.log(res);
+				console.log(result);
 			}
 		});
-		
+
 		app.listen(port, function(){
 			console.log("Server running on http://localhost:"+port);
 			console.log(`Server running on http://localhost:${port}`);
